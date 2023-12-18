d2f06e286c75e899b0c5674fc9674944e0a07e77
Jaspercyt <88648972+Jaspercyt@users.noreply.github.com>
2023-12-18 14:22:36 +0800

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+echo "[TASK 1] set-timezone"
+timedatectl set-timezone Asia/Taipei
+
+echo "[TASK 2] Run the following command to uninstall all conflicting packages"
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+echo "[TASK 3] Uninstall old versions"
+apt-get -y purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+rm -rf /var/lib/docker
+rm -rf /var/lib/containerd
+
+echo "[TASK 4] Set up Docker's apt repository"
+# Add Docker's official GPG key:
+apt-get -y update
+apt-get -y install ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+
+echo "[TASK 5] Install the Docker packages"
+apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "[TASK 6] Install Auto-Complete"
+apt-get update
+apt-get install bash-completion -y
+curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker.sh
+
+echo "[TASK 7] use docker without sudo"
+# 將目前使用者新增到 docker 群組並更改其預設 shell 為 /bin/bash
+usermod -aG docker -s /bin/bash $(whoami)
+# 切換到 docker 群組
+newgrp docker
